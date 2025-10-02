287f16cf96f52b33e10df85ba25c18504b4d34a8
Nareh964 <nareh5490@gmail.com>
2025-10-02 07:58:16 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,23 @@
 let num =[];
 
-function errorMessage(cel){
+function fahrenheitToCelsius(fahr){
+    return (fahr - 32) * 5 / 9;
+}
+
+
+function errorMessage(cel, optional){
     var error = document.getElementById("virhe");
     if (isNaN(cel) || cel === ""){
         error.textContent = "Syötä kelvollisen lämpötila numero "
         error.style.color = "red"
-    } else if(cel < -273.15){
+    } else if(optional === "celsius" && cel < -273.15){
         error.textContent = "Astekko ei saa olla pienempi kuin (-273.15 celsius)."
         error.style.color = "red"
-    } else {
+    }else if(optional === "fahrenheit" && cel < -459.67){
+        error.textContent = "Astekko ei saa olla pienempi kuin (-459.67 fahrenheit)."
+        error.style.color = "red"
+    } 
+    else {
         error.textContent = ""
     }
 }
@@ -17,15 +26,16 @@ function muuntaa(event){
     event.preventDefault();
 
     let input = document.getElementById("astekko");
+    let optional = document.getElementById("conversion").value;
     let cel = parseFloat(input.value.replace(",","."));
-    errorMessage(cel);
+    errorMessage(cel, optional);
 
-    if (isNaN(cel) || cel < -273.15){
+    if ((optional === "celsius" && cel < -273.15) || (optional === "fahrenheit" && cel < -459.67) || isNaN(cel)){
         document.getElementById("tulos").textContent = "";
         return;
     }
 
-    let decimals = 1
+    let decimals = 1;
     let radios = document.getElementsByName("desimaali");
     for (let radio of radios){
         if (radio.checked){
@@ -35,16 +45,20 @@ function muuntaa(event){
             break;
         }
     }
-    let fahr = cel * 9 / 5 + 32;
-    let rounded = fahr.toFixed(decimals);
+    let result, text;
+    if (optional === "celsius"){
+        result = (cel * 9/5) + 32;
+        text = `${cel}°C = ${result.toFixed(decimals)}°F`;
+    } else {
+        result = fahrenheitToCelsius(cel);
+        text = `${cel}°F = ${result.toFixed(decimals)}°C`;
+    }
 
-    document.getElementById("tulos").textContent = `${cel}°C = ${rounded}°F`;
+    document.getElementById("tulos").textContent = text;
 
     num.push(cel);
     input.value = "";
-
-
 }
 
 
-    
+
